54ba095e966ff3ae9fd93a6db972b6606c3cb9c8
Sidhant <2023.sidhant.ramrakhiani@ves.ac.in>
2026-06-19 14:06:50 +0530

    comit

--- app.py
+++ app.py
@@ -876,7 +876,7 @@ def main() -> None:
 
             col1, col2 = st.columns(2)
             with col1:
-                start_date = st.date_input("Start Date", value=datetime(2026, 5, 25).date())
+                start_date = st.date_input("Start Date", value=datetime(2000, 1, 1).date())
             with col2:
                 end_date = st.date_input("End Date", value=datetime(2026, 5, 29).date())
 
